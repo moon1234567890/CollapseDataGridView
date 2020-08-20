dbbe45c5cdea9a132fb2308cff15ea6b9cc51c21
IUL4SGH <IUL4SGH@SZHM-C-0001V>
2020-08-20 14:23:08 +0800

    change child cell edit

--- CollapseDataGridViewTest/Form2.cs
+++ CollapseDataGridViewTest/Form2.cs
@@ -27,62 +27,39 @@ namespace CollapseDataGridViewTest
             var reportKeys = (JObject)obj["ReportKeys"];
             var lowObject = (JObject)reportKeys["Low"];
             this.cdgv_abs.AddColumns((JObject)lowObject["One"].First);
-
+            this.cdgv_abs.CellEndEdit += Cdgv_abs_CellEndEdit;
             cdgv_abs.Columns.Insert(0, SetCheckBoxColumn("LoneCheckBox", "Check"));
             cdgv_abs.Columns.Insert(1, SetButtonColumn("loadOneWithTCS", "Load(WithTCS)", "Load(WithTCS)"));
             cdgv_abs.Columns.Insert(2, SetButtonColumn("loadOneWithOutTCS", "Load(WithOutTCS)", "Load(WithOutTCS)"));
             cdgv_abs.AddData((JArray)lowObject["One"],3,3);
-            //foreach (var item in lowObject["One"])
-            //{
-            //    var childList = new List<JObject>();
-            //    for (int i = 0; i < 3; i++)
-            //    {
-            //        childList.Add((JObject)item);
-            //    }
+        }
 
-            //    CollDataGridViewRow collapseRow = new CollDataGridViewRow();
-            //    collapseRow.IsCollapse = true;
-            //    collapseRow.GroupTag = childList;
+        private void Cdgv_abs_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            var row = this.cdgv_abs.Rows[e.RowIndex];
+            if (!(row is CollDataGridViewRow))
+            {
+                var fatherRowIndex = 0;
+                for (int i = e.RowIndex-1; i >=0; i--)
+                {
+                    if (this.cdgv_abs.Rows[i] is CollDataGridViewRow)
+                    {
+                        fatherRowIndex = i;
+                        break;
+                    }
+                }
+                var fatherRow = this.cdgv_abs.Rows[fatherRowIndex] as CollDataGridViewRow;
+                double fatherValue=0;
+                foreach (var item in fatherRow.Rows)
+                {
 
-            //    DataGridViewCheckBoxCell cell = new DataGridViewCheckBoxCell();
-            //    cell.Value = false;
-            //    collapseRow.Cells.Add(cell);//不预先添加则无法折叠--相当于插入了空行
-
-            //    for (int i=1; i<4; i++)
-            //    {
-            //        DataGridViewRow row = new DataGridViewRow();
-            //        cell = new DataGridViewCheckBoxCell();
-            //        cell.Value = false;
-            //        row.Cells.Add(cell);//不预先添加则无法折叠
-            //        collapseRow.Rows.Add(row);
-            //    }
-
-            //    var rownum = cdgv_abs.Rows.Add(collapseRow);
-            //    cdgv_abs.Expand(rownum);
-
-            //    //Add Data
-            //    for (int i = rownum; i < rownum+4; i++)
-            //    {
-            //        if (i == rownum)
-            //        {
-            //            DataGridViewTextBoxCell cellTextBox = new DataGridViewTextBoxCell();
-            //            cellTextBox.Value = "";
-            //            this.cdgv_abs.Rows[i].Cells[0]= cellTextBox;
-            //            DataGridViewTextBoxCell cellTextBox2 = new DataGridViewTextBoxCell();
-            //            cellTextBox2.Value = "";
-            //            this.cdgv_abs.Rows[i].Cells[1] = cellTextBox2;
-            //            DataGridViewTextBoxCell cellTextBox3 = new DataGridViewTextBoxCell();
-            //            cellTextBox3.Value = "";
-            //            this.cdgv_abs.Rows[i].Cells[2] = cellTextBox3;
-            //        }
-            //        int j = 3;
-            //        foreach (JProperty item2 in item)
-            //        {
-            //            this.cdgv_abs.Rows[i].Cells[j].Value = item2.Value;
-            //            j++;
-            //        }
-            //    }
-            //}
+                    if (item.Cells[e.ColumnIndex].Value != null&&!string.IsNullOrEmpty(item.Cells[e.ColumnIndex].Value.ToString()))
+                    {
+                        fatherValue += Convert.ToDouble(item.Cells[e.ColumnIndex].Value.ToString());
+                    }
+                }
+                fatherRow.Cells[e.ColumnIndex].Value = fatherValue;
+            }
         }
 
         private DataGridViewButtonColumn SetButtonColumn(string name, string headerText, string value)
